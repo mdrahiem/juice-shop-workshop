ebb47e15e2bcd69cc048f4cca37380586fe7f2e2
rahimuddin.mohammad <rahimuddin.mohammad@postcodeloterij.nl>
2025-12-03 14:11:22 +0100

    couple more fixes

--- routes/logfileServer.ts
+++ routes/logfileServer.ts
@@ -8,10 +8,11 @@ import { type Request, type Response, type NextFunction } from 'express'
 
 module.exports = function serveLogFiles () {
   return ({ params }: Request, res: Response, next: NextFunction) => {
-    const file = params.file
+    const file = path.basename(path.resolve('logs/', params.file))
 
-    if (!file.includes('/')) {
-      res.sendFile(path.resolve('logs/', file))
+
+      res.sendFile(path.resolve('logs/', path.basename(file)))
+      res.sendFile(path.resolve('logs/', path.basename(file)))
     } else {
       res.status(403)
       next(new Error('File names cannot contain forward slashes!'))
--- routes/vulnCodeFixes.ts
+++ routes/vulnCodeFixes.ts
@@ -76,8 +76,8 @@ export const checkCorrectFix = () => async (req: Request<Record<string, unknown>
     })
   } else {
     let explanation
-    if (fs.existsSync('./data/static/codefixes/' + key + '.info.yml')) {
-      const codingChallengeInfos = yaml.load(fs.readFileSync('./data/static/codefixes/' + key + '.info.yml', 'utf8'))
+    if (fs.existsSync('./data/static/codefixes/' + path.basename(key) + '.info.yml')) {
+      const codingChallengeInfos = yaml.load(fs.readFileSync('./data/static/codefixes/path.js', 'utf8'))
       const selectedFixInfo = codingChallengeInfos?.fixes.find(({ id }: { id: number }) => id === selectedFix + 1)
       if (selectedFixInfo?.explanation) explanation = res.__(selectedFixInfo.explanation)
     }
